9d0ee3e9bc369c40e828b303d3e472b2393572f2
Cory Patten <Cannedcornflakes@gmail.com>
2022-12-14 10:17:00 -0700

    Added sequential renamer

---
+++ SequentialRenamer.py
@@ -0,0 +1,22 @@
+sequenceName = str(input("Sequence Name: "))
+sequenceLength = int(input("Number of objects you are renaming: "))
+
+def SeqRenamer(txt, num):
+
+    count = txt.count("#")
+
+    poundFind = txt.find("#" * count)
+    if poundFind < 0:
+        print("Enter the sequence with all the pound signs together")
+        return
+
+    for i in range(1, num+1):
+        x = txt.replace("#" * count, str(i).zfill(count))
+        print(x)
+
+SeqRenamer(sequenceName, sequenceLength)
+
+#leg_#####_jnt_###
+
+#leg_00000001_jnt
+#leg_000000_jnt_01
